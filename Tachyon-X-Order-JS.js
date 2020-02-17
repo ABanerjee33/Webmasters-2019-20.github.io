2f8f86b7fab881e1bd0a420aff8136682497ce91
AkhileshS123 <44590495+AkhileshS123@users.noreply.github.com>
2020-02-17 10:58:10 -0500

    Add files via upload

--- Tachyon-X-Order-JS.js
+++ Tachyon-X-Order-JS.js
@@ -154,10 +154,17 @@ function updateImages(){
   } else {
     imageString = "https://i.ibb.co/p2HsTw4/Tire2-White.png";
   }
-  alert("The update images method is called");
+  //alert("The update images method is called");
+  var imageElements = document.getElementsByClassName("Car-picture");
+  alert("The length of imageElements should be 5 is : " + imageElements.length);
+  for(var i = 0; i < imageElements.length; i++){
+    imageElements[i].src = imageString;
+  }
+  /*
   document.getElementById("Car-Picture").src=imageString;
   document.getElementById("Summary-Image").src=imageString;
   document.getElementById("Mobile-Exterior-Image").src=imageString;
+  */
   //sessionStorage.setItem("Car-Picture", imageString);
 }
 
