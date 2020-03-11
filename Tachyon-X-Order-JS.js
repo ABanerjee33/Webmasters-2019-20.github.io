33d578ff74495836440889bf6982f51945448356
AkhileshS123 <akhilesh.sivaganesan@gmail.com>
2020-03-11 15:03:29 -0400

    Commented Static Prick block

--- Tachyon-X-Order-JS.js
+++ Tachyon-X-Order-JS.js
@@ -218,9 +218,11 @@ function assignFeature_Objects(buttonName){
         	buttonEl.children[0].textContent = buttonEl.individual_object.getName();
           buttonEl.children[1].textContent = buttonEl.individual_object.getPrice();
         } else {
+          /*
           var description = document.getElementById(buttonEl.getGroup());
           description.children[0].textContent = buttonEl.individual_object.getName();
           description.children[1].textContent = buttonEl.individual_object.getPrice();
+          */
         }
         if(buttonEl.individual_object.getState()){
           buttonEl.classList.add("active");
