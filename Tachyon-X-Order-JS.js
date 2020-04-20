14d9f38e415cd976514baf6627c19bb628933b37
AkhileshS123 <akhilesh.sivaganesan@gmail.com>
2020-04-20 12:12:13 -0400

    Checking Number of Horizontal Flex
    Should be 6.

--- Tachyon-X-Order-JS.js
+++ Tachyon-X-Order-JS.js
@@ -439,7 +439,7 @@ function assignFeature_Objects(buttonName){
           if(buttonEl.individual_object.getState()){
             var roundDescs = document.getElementsByClassName(buttonEl.individual_object.getGroup().toLowerCase());
             alert(buttonEl.individual_object.getGroup() + " " + roundDescs.length);
-            alert(document.getElementsByClassName("Horizontal-Flex").length);
+            alert(document.getElementsByClassName("horizontal-flex").length);
             for(var k = 0; k < roundDescs.length; k++){
               var roundDesc = roundDescs[k];
               roundDesc.children[0].textContent = buttonEl.individual_object.getName();
